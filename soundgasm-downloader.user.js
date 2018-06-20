d2f0901e53a5064e0c6e052ce81089a5201fcff0
RobertSagit <40412562+RobertSagit@users.noreply.github.com>
2018-06-19 21:25:46 -0400

    Create soundgasm-downloader.user.js

---
+++ soundgasm-downloader.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Soundgasm Downloader
+// @namespace    http://jacenboy.ml/
+// @version      1.0
+// @description  Add a download link to Soundgasm.net links
+// @author       JacenBoy
+// @match        https://soundgasm.net/u/*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function gendownload() {
+        var audioplayer = document.getElementById("jp_audio_0");
+        var description = document.getElementsByClassName("jp-description")[0];
+        var title = document.getElementsByClassName("jp-title")[0].innerHTML.replace(/[^a-z0-9]/gi, '');
+
+        // Debug code
+        //console.log(`Audio source: ${audioplayer.src}`);
+
+        description.innerHTML += `<br /><a href="${audioplayer.src}" download="${title}">Download File</a>`;
+    }
+
+    setTimeout(gendownload, 100);
+})();
